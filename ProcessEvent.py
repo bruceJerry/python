91a3fc4a180049f86f27a0dc34fc1379402e9a3c
bruceJerry <Zwy123456j>
2018-09-03 18:08:25 +0800

    不同空间之间通过signal和slot通信

--- ProcessEvent.py
+++ ProcessEvent.py
@@ -9,12 +9,25 @@ class MyWindows(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("ProcessEvent")
-        self.setGeometry(200, 200, 400, 100)
+        self.setGeometry(200, 200, 400, 400)
         self.show()
         self.ui_init()
 
     def ui_init(self):
-        pass
+        self.listWidget = QListWidget()
+        self.btn_start = QPushButton("开始")
+        layout = QGridLayout()
+        layout.addWidget(self.listWidget)
+        layout.addWidget(self.btn_start)
+        self.btn_start.clicked.connect(self.clicked)
+        self.setLayout(layout)
+
+    def clicked(self):
+        for n in range(10):
+            str_n = "file {}".format(n)
+            self.listWidget.addItem(str_n)
+            QApplication.processEvents()
+            time.sleep(1)
 
 
 app = QApplication(["hello"])
